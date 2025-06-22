8bbe07a070f5621abd5ff26f73da84faf2af04c5
Davi Marcelino <marcelinodavi265@gmail.com>
2025-06-22 18:41:50 -0300

    TCP

--- sever.js
+++ sever.js
@@ -4,13 +4,13 @@ const HOST = 'localhost';
 const PORT = 5000;
 
 const server = net.createServer((socket) => {
-    console.log('Servidor rodando...');
+    console.log('Servidor rodando');
     console.log(`Conexão aceita de ${socket.remoteAddress}:${socket.remotePort}`);
     
     socket.on('data', (data) => {
         const message = data.toString();
         console.log(`Mensagem recebida: ${message}`);
-        socket.write('Mensagem recebida!');
+        socket.write('Mensagem recebida');
     });
 
     socket.on('end', () => {
